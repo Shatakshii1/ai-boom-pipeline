0a986fd817b30629801fbf8d42447527f0884881
Shatakshi Dixit <Shatakshidixt@gmail.com>
2026-05-21 15:21:36 -0400

    ingestion: added ingestion summary documentation

---
+++ ingestion/01_ingestion_summary.sql
@@ -0,0 +1,22 @@
+/*
+FILE: 01_ingestion_summary.sql
+PURPOSE: Documents all raw tables in ingestion layer
+
+SCHEMA: ingestion_funding
+  raw_funding_rounds -> Source: Kaggle - Global Startup Funding Rounds 2020-2026
+  raw_funding_startups -> Source: Kaggle - Global Startup Funding Rounds 2020-2026
+  raw_investors -> Source: Kaggle - Global Startup Funding Rounds 2020-2026
+  raw_quarterly_summary -> Source: Kaggle - Global Startup Funding Rounds 2020-2026
+
+SCHEMA: ingestion_layoffs
+  raw_layoffs -> Source: Kaggle - Tech Layoffs 2020-2025
+  raw_layoffs_locations -> Source: Kaggle - Tech Layoffs 2020-2025
+
+SCHEMA: ingestion_startups
+  raw_crunchbase -> Source: Kaggle - Big Startup Success/Fail Dataset
+
+SCHEMA: ingestion_reddit
+  raw_reddit_ds -> Source: Kaggle - Reddit Data Science Posts 500k+
+  raw_reddit_tech -> Source: Kaggle - Reddit r/technology
+
+*/
