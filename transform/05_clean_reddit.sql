49f7ff603a27b66b7b5cbd3027c9b56894100121
Shatakshi Dixit <Shatakshidixt@gmail.com>
2026-05-22 21:30:33 -0400

    transform: cleaned reddit tech table with year_month and engagement_score

--- transform/05_clean_reddit.sql
+++ transform/05_clean_reddit.sql
@@ -1,3 +1,4 @@
+/* Source: Reddit Data Science Posts 500k+ */
 CREATE TABLE transform_reddit.clean_reddit_ds AS
 SELECT 
   DATE_FORMAT(created_date, 'yyyy-MM') AS year_month,
@@ -15,9 +16,24 @@ SELECT
   subreddit_subscribers,
   post
 FROM ingestion_reddit.raw_reddit_ds
---Give me any posts that belong to any of these 8 subreddits
 WHERE
   subreddit IN ("MachineLearning", "datascience", "dataengineering", "artificial", "learnmachinelearning", "DataScienceJobs", "MLQuestions", "deeplearning") AND
   YEAR(created_date) >= 2020 AND
   title IS NOT NULL AND
   id IS NOT NULL;
+
+
+/* Source: Reddit r/technology Submissions & Comments */
+CREATE TABLE transform_reddit.clean_reddit_tech AS
+SELECT 
+  title,
+  score,
+  id,
+  url,
+  (score * 1 + comms_num * 5) AS engagement_score,
+  comms_num,
+  created,
+  body,
+  DATE_FORMAT(timestamp, "yyyy-MM") AS year_month
+FROM ingestion_reddit.raw_reddit_tech
+WHERE title IS NOT NULL AND id IS NOT NULL;
